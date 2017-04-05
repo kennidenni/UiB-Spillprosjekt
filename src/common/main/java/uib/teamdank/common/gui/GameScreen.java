3b1fabece334e7f39945681d2f3941220dda8565
peterjohansen <peterandrejohansen@gmail.com>
2017-04-05 15:08:00 +0200

    Change GameScreen to class

--- src/common/main/java/uib/teamdank/common/gui/GameScreen.java
+++ src/common/main/java/uib/teamdank/common/gui/GameScreen.java
@@ -8,13 +8,53 @@ import com.badlogic.gdx.Screen;
  * Weather data, procedural generation and other nifty classes can be found in
  * the {@link uib.teamdank.common.util}-package.
  */
-public interface GameScreen extends Screen {
+public class GameScreen implements Screen {
 
-	public void addLayer(Layer layer);
+	public void addLayer(Layer layer) {
+		
+	}
 	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	/**
 	 * Pauses the game and shows a {@link PauseMenuScreen}.
 	 */
-	public void pause();
+	public void pause() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
