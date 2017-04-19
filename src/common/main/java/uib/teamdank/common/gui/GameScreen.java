ae56cfaa7c41304d5bc385fdc287b3a9039cb798
Peter André Johansen <peterandrejohansen@gmail.com>
2017-04-19 19:51:05 +0200

    Removed failing test that is now runtime-dependent

--- src/common/main/java/uib/teamdank/common/gui/GameScreen.java
+++ src/common/main/java/uib/teamdank/common/gui/GameScreen.java
@@ -76,6 +76,7 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		
+		System.out.println(Gdx.gl);
 		Gdx.gl.glClearColor( 0, 1, 0, 1 );
 		Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
 		
