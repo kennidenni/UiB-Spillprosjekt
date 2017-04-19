48f73899fd37a0690687e428b06518555e837255
peterjohansen <peterandrejohansen@gmail.com>
2017-04-19 16:27:25 +0200

    Work on ScrollingImage

--- src/car-game/main/java/uib/teamdank/cargame/ScrollingImage.java
+++ src/car-game/main/java/uib/teamdank/cargame/ScrollingImage.java
@@ -1,16 +1,20 @@
 package uib.teamdank.cargame;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ScrollingImage {
+	private final OrthographicCamera camera;
 	private final Texture texture;
 	
-	public ScrollingImage(Texture texture) {
+	public ScrollingImage(OrthographicCamera camera, Texture texture) {
+		this.camera = camera;
 		this.texture = texture;
 	}
 	
-	public void render(float delta) {
-		
+	public void render(SpriteBatch batch, float delta) {
+		batch.draw(texture, 0, 0);
 	}
 
 	public void update(float delta) {
--- src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
@@ -1,6 +1,7 @@
 package uib.teamdank.cargame.gui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -8,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import uib.teamdank.cargame.Player;
+import uib.teamdank.cargame.ScrollingImage;
 import uib.teamdank.common.Game;
 import uib.teamdank.common.gui.Layer;
 
@@ -24,12 +26,14 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 	private final Player player = new Player();
 	
 	private final Texture backgroundTexture;
+	private final ScrollingImage scrollingRoad;
 	
 	public GameScreen(Game game) {
 		super(game);
 		
-		// Background texture
+		// Background
 		backgroundTexture = new Texture(Gdx.files.internal("Images/background.png"));
+		scrollingRoad = new ScrollingImage(camera, new Texture(Gdx.files.internal("Images/road.png")));
 		
 		// Player
 		player.setTexture(new TextureRegion(new Texture(Gdx.files.internal("Images/car.png"))));
@@ -47,14 +51,16 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 		final int screenWidth = Gdx.graphics.getWidth();
 		final int screenHeight = Gdx.graphics.getHeight();
 		
+		// Draw background
 		SpriteBatch batch = getGame().getSpriteBatch();
 		batch.begin();
 		batch.draw(backgroundTexture, 0, 0, screenWidth, screenHeight);
+		scrollingRoad.render(batch, delta);
 		batch.end();
 		
+		// Camera
 		Vector2 playerPos = player.getPosisiton();
 		camera.position.set(playerPos.x + player.getWidth() / 2, playerPos.y + player.getHeight() / 2, 0);
-		
 		camera.viewportWidth = screenWidth;
 		camera.viewportHeight = screenHeight;
 		camera.update();
@@ -66,7 +72,7 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 	@Override
 	public void update(float delta) {
 		super.update(delta);
-		
+		scrollingRoad.update(delta);
 	}
 
 }
