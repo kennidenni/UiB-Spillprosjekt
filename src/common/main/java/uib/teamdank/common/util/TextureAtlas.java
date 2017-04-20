613eef62901a9750abe8c6be450d4b24e769c556
Peter André Johansen <peterandrejohansen@gmail.com>
2017-04-20 20:04:32 +0200

    Finish texture atlas

--- src/common/main/java/uib/teamdank/common/util/TextureAtlas.java
+++ src/common/main/java/uib/teamdank/common/util/TextureAtlas.java
@@ -1,31 +1,68 @@
 package uib.teamdank.common.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 
+/**
+ * A texture atlas manages named regions on a larger tileset which can be
+ * retrieved by calling {@link #getRegion(String)}.
+ * <p>
+ * <b>Note:</b>When {@link #load()} is called the entire tileset image
+ * is loaded into memory. Call {@link #dispose()} when done.
+ */
 public class TextureAtlas {
 
-	public static void main(String[] args){
-//		Gdx.files.external("/Desktop/atlas.json")
-		TextureAtlas a = TextureAtlas.loadAtlas(Gdx.files.internal("/atlas.json"));
-		System.out.println(TextureAtlas.class.getResourceAsStream("atlas.json"));
+	public static TextureAtlas createFromJson(FileHandle handle){
+		return new Gson().fromJson(handle.reader(), TextureAtlas.class);
 	}
-	
-	private Region[] regions;
 	
 	private static class Region {
 		String name;
 		int x;
 		int y;
-		int width;
-		int height;
+		int w;
+		int h;
 	}
 	
-	public static TextureAtlas loadAtlas(FileHandle handle){
-		Gson gson = new Gson();
-		
-		TextureAtlas atlas = gson.fromJson(handle.reader(), TextureAtlas.class);
-		return atlas;
+	@SerializedName("tileset") private String tilesetFile;	
+	@SerializedName("regions") private Region[] regions;
+	
+	private transient  Map<String, TextureRegion> textureCache = new HashMap<>();
+	private transient Texture tileset;
+	
+	/**
+	 * @return the texture region with the given name 
+	 */
+	public TextureRegion getRegion(String name) {
+		if (tileset == null) {
+			throw new IllegalStateException("texture atlas has not loaded the tileset yet, call #load()");
+		}
+		if (!textureCache.containsKey(name)) {
+			throw new IllegalArgumentException("no such region: " + name);
+		}
+		return textureCache.get(name);
 	}
+	
+	public void load() {
+		FileHandle sheetFile = Gdx.files.internal(tilesetFile);
+		if (sheetFile == null) {
+			throw new IllegalArgumentException("no such tileset: " + tilesetFile);
+		}
+		this.tileset = new Texture(sheetFile);
+		for (Region region : regions) {
+			textureCache.put(region.name, new TextureRegion(tileset, region.x, region.y, region.w, region.h));
+		}
+	}
+	
+	public void dispose() {
+		tileset.dispose();
+	}
+	
 }
---
+++ src/common/test/java/uib/teamdank/common/TextureAtlasTest.java
@@ -0,0 +1,58 @@
+package uib.teamdank.common;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import uib.teamdank.common.util.TextureAtlas;
+
+public class TextureAtlasTest {
+
+	public static void main(String[] args){
+		new LwjglApplication(new Game() {
+			private static final float BIRD_SCALE = 12f;
+			
+			private SpriteBatch batch;
+			private TextureAtlas atlas;
+			
+			private Animation<TextureRegion> birdAnim;
+			private float time = 0;
+			
+			@Override
+			public void create() {
+				this.batch = new SpriteBatch();
+				this.atlas = TextureAtlas.createFromJson(Gdx.files.internal("bird_atlas.json"));
+				atlas.load();
+				
+				this.birdAnim = new Animation<>(.1f, atlas.getRegion("bird1"),
+														atlas.getRegion("bird2"),
+														atlas.getRegion("bird3"),
+														atlas.getRegion("bird4"),
+														atlas.getRegion("bird5"));
+				birdAnim.setPlayMode(PlayMode.LOOP);
+			}
+			
+			public void render() {
+				Gdx.gl.glClearColor( 0, 0, 0, 1 );
+				Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+				
+				time += Gdx.graphics.getDeltaTime();
+				
+				final TextureRegion frame = birdAnim.getKeyFrame(time);
+				final int w = (int) (frame.getRegionWidth() * BIRD_SCALE);
+				final int h = (int) (frame.getRegionHeight() * BIRD_SCALE);
+				final int x = (Gdx.graphics.getWidth() - w) / 2;
+				final int y = (Gdx.graphics.getHeight() - h) / 2;
+				batch.begin();
+				batch.draw(frame, x, y, w, h);
+				batch.end();
+			}
+		});
+	}
+	
+}
