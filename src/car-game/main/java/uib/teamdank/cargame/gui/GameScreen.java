9c5a78fef95f403295d1479d2a6930cc12c07886
Peter André Johansen <peterandrejohansen@gmail.com>
2017-04-21 17:53:34 +0200

    Prevent car from off-roading

--- src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
@@ -15,48 +15,48 @@ import uib.teamdank.common.util.TextureAtlas;
  */
 public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 	private static final int CAR_VERTICAL_POSITION = 25;
-	
+
 	private static final float CAR_HORIZONTAL_ACCELERATION = 50f;
 	private static final float CAR_VERTICAL_SPEED = 512f;
 	private static final float CAR_HORIZONTAL_FRICTION = .9f;
-	
+
 	private final OrthographicCamera playerCamera;
 	private final OrthographicCamera screenCamera;
-	
-	private final Layer backgroundLayer;
+
+	private final BackgroundLayer backgroundLayer;
 	private final Layer carLayer;
-	
+
 	private final Player player;
-	
+
 	public GameScreen(Game game) {
 		super(game);
-		
+
 		TextureAtlas gameObjectTextures = TextureAtlas.createFromJson(Gdx.files.internal("Images/game_objects.json"));
-		
+
 		// Cameras
 		this.playerCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		this.screenCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		
+
 		// Player initialization
 		player = new Player();
 		player.setTexture(gameObjectTextures.getRegion("car_forward"));
 		player.setScale(.5f);
 		player.getVelocity().y = CAR_VERTICAL_SPEED;
-		
+
 		// Layers
 		backgroundLayer = new BackgroundLayer(playerCamera, screenCamera, player);
 		carLayer = new Layer(true);
 		addLayer(backgroundLayer);
 		addLayer(carLayer);
 		carLayer.addGameObject(player);
-		
+
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		final int screenWidth = Gdx.graphics.getWidth();
 		final int screenHeight = Gdx.graphics.getHeight();
-		
+
 		// Update player camera
 		Vector2 playerPos = player.getPosisiton();
 		float cameraX = playerPos.x + player.getWidth() / 2;
@@ -66,25 +66,32 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 		playerCamera.viewportHeight = screenHeight;
 		playerCamera.update();
 		getGame().getSpriteBatch().setProjectionMatrix(playerCamera.combined);
-		
+
 		// Render layers
 		super.render(delta);
-		
+
 	}
-	
+
 	@Override
 	public void update(float delta) {
-		
+
 		// Update game objects
 		super.update(delta);
-		
+
 		// Player movement
 		boolean left = Gdx.input.isKeyPressed(Keys.A);
 		boolean right = Gdx.input.isKeyPressed(Keys.D);
 		if (left) player.getVelocity().x -= CAR_HORIZONTAL_ACCELERATION;
 		if (right) player.getVelocity().x += CAR_HORIZONTAL_ACCELERATION;
 		player.getVelocity().x *= CAR_HORIZONTAL_FRICTION;
-		
+		if (player.getPosisiton().x < backgroundLayer.getRoadLeftX()) {
+			player.getPosisiton().x = backgroundLayer.getRoadLeftX();
+			player.getVelocity().x *= -1;
+		} else if (player.getPosisiton().x > backgroundLayer.getRoadRightX() - player.getWidth()) {
+			player.getPosisiton().x = backgroundLayer.getRoadRightX() - player.getWidth();
+			player.getVelocity().x *= -1;
+		}
+
 	}
 
 }
