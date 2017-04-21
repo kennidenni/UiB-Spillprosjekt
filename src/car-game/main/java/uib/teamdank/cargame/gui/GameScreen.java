918743e8388ae146160dc1b11462a0c9f838e5dd
Peter André Johansen <peterandrejohansen@gmail.com>
2017-04-21 19:42:00 +0200

    Decrease car fuel gradually as test

--- src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/GameScreen.java
@@ -104,7 +104,7 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 		
 		// Update HUD
 		hud.setCurrentFuel(player.getHealth());
-		player.decreaseHealth(75);
+		player.setHealth(player.getMaxHealth() - (int) (player.getPosisiton().y / 100));
 
 	}
 
