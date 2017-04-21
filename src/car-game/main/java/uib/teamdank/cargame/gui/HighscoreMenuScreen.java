25fab06e937309c1734f6bae69db5539bc399f88
Håvar Eggereide <havar.eggereide@student.uib.no>
2017-04-21 15:20:37 +0200

    Tok bort ubrukt import

--- src/car-game/main/java/uib/teamdank/cargame/gui/HighscoreMenuScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/HighscoreMenuScreen.java
@@ -1,8 +1,6 @@
 package uib.teamdank.cargame.gui;
 
 import java.util.List;
-
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
