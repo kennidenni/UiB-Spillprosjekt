2228c0d76a61011e42a05f6d04f0f3b887175040
peterjohansen <peterandrejohansen@gmail.com>
2017-04-25 11:33:36 +0200

    Add worst progress bar ever

--- src/car-game/main/java/uib/teamdank/cargame/gui/CarHud.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/CarHud.java
@@ -1,17 +1,21 @@
 package uib.teamdank.cargame.gui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class CarHud {
-	private static final String FUELTANK = "images/game/gastank.png";
+	private static final String FUELTANK = "Images/Game/gastank.png";
 
 	private int fuel;
 	private Stage stage;
@@ -24,19 +28,31 @@ public class CarHud {
 		stage = new Stage(new FitViewport(1920, 1080));
 
 		fuelImage = setupButton(FUELTANK);
+		
+		Skin skin = new Skin();
+		Pixmap pixmap = new Pixmap(40, 500, Format.RGBA8888);
+		pixmap.setColor(Color.RED);
+		pixmap.fill();
+		skin.add("red", new Texture(pixmap));
+		
+		pixmap = new Pixmap(40, 500, Format.RGBA8888);
+		pixmap.setColor(Color.GREEN);
+		pixmap.fill();
+		skin.add("green", new Texture(pixmap));
 
 		ProgressBar.ProgressBarStyle style = new ProgressBar.ProgressBarStyle();
-		style.background = new TextureRegionDrawable(new TextureRegion(new Texture("images/road.png")));
-		//style.knob = new TextureRegionDrawable(new TextureRegion(new Texture("images/road.png")));
-		//style.knobBefore = style.knob;
+		style.background = skin.getDrawable("red");
+		style.knobBefore = skin.getDrawable("green");
+//		style.background = new TextureRegionDrawable(new TextureRegion(new Texture("Images/road.png")));
+//		style.knobBefore = new TextureRegionDrawable(new TextureRegion(new Texture("Images/background.png")));
+//		style.knobBefore = style.knob;
 
-		bar = new ProgressBar(0, 100, 1, false, style);
-		bar.setRange(1, 100);
+		bar = new ProgressBar(1, 100, 1, false, style);
 		bar.setValue(fuel);
-
+		
 		menu = new Table();
 		menu.add(fuelImage).width((float) (fuelImage.getWidth() / 2)).height((float) (fuelImage.getHeight() / 2)).pad(900, 1650, 0, 0);
-		menu.add(bar).width(bar.getWidth() / 2).pad(900, 10, 100, 50);
+		menu.add(bar).width(bar.getWidth()).pad(900, 10, 100, 50);
 
 		menu.setFillParent(true);
 		stage.addActor(menu);
