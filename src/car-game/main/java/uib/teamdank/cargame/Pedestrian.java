83d9cc37ee88e667dae98de795ae51dec5ca9082
Elias Siljan <Elias.Siljan@student.uib.no>
2017-04-26 17:09:11 +0200

    test commit(ignore this)

--- src/car-game/main/java/uib/teamdank/cargame/Pedestrian.java
+++ src/car-game/main/java/uib/teamdank/cargame/Pedestrian.java
@@ -3,6 +3,7 @@ package uib.teamdank.cargame;
 import uib.teamdank.cargame.gui.GameScreen;
 import uib.teamdank.common.Actor;
 
+
 /**
  * A pedestrian in the road. Has positive effects if driven over by the
  * {@link Player}.
