84a5d0e16a53fbcce17d0ede01dea7da22380020
Kenneth Apeland <Kenneth Apeland>
2017-04-27 22:23:02 +0200

    kan se om de er unlocked og kan klikke på de

--- src/car-game/main/java/uib/teamdank/cargame/gui/ShopScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/ShopScreen.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -25,39 +26,42 @@ public class ShopScreen implements Screen {
 	private static final String BACK = "Images/Buttons/bs_back.png";
 	
 	private Stage stage;
-	private Table menu;
 	private CarGame game;
 	private TextureAtlas carTextures;
 	private Array<Button> buttons;
 	private AssetManager assets;
 	
 	private ImageButton backButton;
-	private ImageButton greenCar;
-	private ImageButton blueCar;
-	private ImageButton norwayCar;
-	private ImageButton pinkCar;
-	private ImageButton redCar;
-	private ImageButton greyCar;
-	private ImageButton flameCar;
-	private ImageButton yellowCar;
-	private ImageButton splatCar;
+	private CarButton greenCar;
+	private CarButton blueCar;
+	private CarButton norwayCar;
+	private CarButton pinkCar;
+	private CarButton redCar;
+	private CarButton greyCar;
+	private CarButton flameCar;
+	private CarButton yellowCar;
+	private CarButton splatCar;
 
+	private Table menu;
 	private Table cars;
-
 	private Table text;
-
+	
 	public ShopScreen(CarGame game) {
 		this.game = game;
 		stage = new Stage(new FitViewport(1920, 1080));
 		
+		buttons = new Array<Button>();
+		menu = new Table();
+		cars = new Table();
+		text = new Table();
+		
 		this.assets = new AssetManager();
+		carTextures = assets.getAtlas("Images/car_sheet.json");
+		
 		Texture backStart = new Texture(Gdx.files.internal(BACK));
 		TextureRegion myTextureRegion = new TextureRegion(backStart);
 		TextureRegionDrawable myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
 		backButton = new ImageButton(myTexRegionDrawable);
-		
-		carTextures = assets.getAtlas("Images/car_sheet.json");
-		buttons = new Array<Button>();
 		
 		buttons.add(greenCar = setupButton("car_forward"));
 		buttons.add(blueCar = setupButton("car_forward_blue"));
@@ -69,10 +73,18 @@ public class ShopScreen implements Screen {
 		buttons.add(yellowCar = setupButton("car_forward_orange"));
 		buttons.add(splatCar = setupButton("car_forward_splat"));
 		
-		menu = new Table();
-		cars = new Table();
-		text = new Table();
+		setupScreen();
+		backListener();
+		carListener();
 		
+		greenCar.setUnlocked(true);
+		
+		menu.setFillParent(true);
+		stage.addActor(menu);
+		Gdx.input.setInputProcessor(stage);
+	}
+
+	public void setupScreen() {
 		for (int i = 0; i < buttons.size; i++) {
 			if (i % 3 == 0) 
 	    		cars.row();
@@ -82,51 +94,12 @@ public class ShopScreen implements Screen {
 		menu.row();
 		menu.add(backButton).width((float) (backButton.getWidth() / 4)).height((float) (backButton.getHeight() / 4)).pad(0, 0, 0, 0);
 		menu.debug();
-		
-		
-		
-		menu.setFillParent(true);
-		stage.addActor(menu);
-		Gdx.input.setInputProcessor(stage);
-		
-		backButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Stage myStage = event.getTarget().getStage();
-				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
-
-				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
-					game.setScreen(game.getStartMenuScreen());
-				}
-			}
-		});
-		redCar.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Stage myStage = event.getTarget().getStage();
-				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
-
-				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
-					System.out.println("heri");
-				}
-			}
-		});
 	}
 
-	public ImageButton setupButton(String string) {
+	public CarButton setupButton(String string) {
 		TextureRegion textureRegion = carTextures.getRegion(string);
 		TextureRegionDrawable myTexRegionDrawable = new TextureRegionDrawable(textureRegion);
-		return new ImageButton(myTexRegionDrawable);
+		return new CarButton(myTexRegionDrawable, true);
 	}
 	
 	@Override
@@ -146,32 +119,241 @@ public class ShopScreen implements Screen {
 	public void resize(int width, int height) {
 		stage.getViewport().update(width, height, true);
 	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public void hide() {
 		Gdx.input.setInputProcessor(null);
 	}
 
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
+	public void pause() { }
+
+	@Override
+	public void resume() { }
+
+	@Override
+	public void dispose() { }
+	
+	private void backListener() {
+		backButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					game.setScreen(game.getStartMenuScreen());
+				}
+			}
+		});
+	}
+	
+	private void carListener() {
+		greenCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					
+					if (greenCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("green");
+					}
+				}
+			}
+		});
 		
+		blueCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (blueCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("blue");
+					}
+				}
+			}
+		});
+		
+		norwayCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (norwayCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("norway");
+					}
+				}
+			}
+		});
+		
+		pinkCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (pinkCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("pink");
+					}
+				}
+			}
+		});
+		
+		redCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (redCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("red");
+					}
+				}
+			}
+		});
+		
+		greyCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (greyCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("grey");
+					}
+				}
+			}
+		});
+		
+		flameCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (flameCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("flameCar");
+					}
+				}
+			}
+		});
+		
+		yellowCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (yellowCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("yellow");
+					}
+				}
+			}
+		});
+		
+		splatCar.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				Stage myStage = event.getTarget().getStage();
+				Vector2 mouse = myStage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+
+				if (myStage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
+					if (splatCar.isUnlocked()) {
+						//player will select this car
+						System.out.println("splat");
+					}
+				}
+			}
+		});
 	}
 
-	
 	public void goBack() {
 		game.setScreen(game.getStartMenuScreen());
+	}
+	
+	private static class CarButton extends ImageButton {
+		public boolean unlocked = false;
+		
+		public CarButton(Drawable imageUp, boolean unlocked) {
+			super(imageUp);
+			this.unlocked = unlocked;
+		}
+		
+		public void setUnlocked(boolean lock) {
+			this.unlocked = lock;
+		}
+		
+		public boolean isUnlocked() {
+			return unlocked;
+		}
 	}
 }
