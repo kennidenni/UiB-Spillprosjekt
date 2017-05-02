7925d80a50f87a47d83ec6d3e193ddd9561036a3
Kenneth Apeland <kenape96@outlook.com>
2017-05-02 11:14:55 +0200

    startet såvidt

--- src/car-game/main/java/uib/teamdank/cargame/gui/CarHud.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/CarHud.java
@@ -17,23 +17,36 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
+import uib.teamdank.common.util.AssetManager;
+import uib.teamdank.common.util.TextureAtlas;
+
 public class CarHud {
-	private static final String FUELTANK = "Images/Game/gastank.png";
 	
 	private Stage stage;
-	private Table menu;
+	private AssetManager assets;
 	private ImageButton fuelImage;
+	private ImageButton coinImage;
 	private ProgressBar bar;
+	
 	private BitmapFont font;
 	private TextButtonStyle textButtonStyle;
 	private TextButton highscore;
+	private TextButton coinsCount;
+	
+	private Table menu;
 	private Table first;
 	private Table second;
+	private Table coinsTable;
+	
 
 	public CarHud() {
 		stage = new Stage(new FitViewport(1920, 1080));
+		
+		this.assets = new AssetManager();
+		TextureAtlas roadEntityTextures = assets.getAtlas("Images/road_entity_sheet.json");
 
-		fuelImage = setupImage(FUELTANK);
+		fuelImage = setupImage(roadEntityTextures.getRegion("gastank"));
+		coinImage = setupImage(roadEntityTextures.getRegion("coin"));
 		
 		Skin skin = new Skin();
 		Pixmap pixmap = new Pixmap(40, 110, Format.RGBA8888);
@@ -59,6 +72,9 @@ public class CarHud {
 		highscore = new TextButton("0", textButtonStyle);
 		highscore.getLabel().setFontScale(10, 10);
 		
+		coinsCount = new TextButton("0", textButtonStyle);
+		coinsCount.getLabel().setFontScale(10, 10);
+		
 		menu = new Table();
 		first = new Table();
 		first.add(highscore).width(300).pad(900, 20, 0, 0);
@@ -67,24 +83,29 @@ public class CarHud {
 		second.add(fuelImage).width((float) (fuelImage.getWidth() / 2)).height((float) (fuelImage.getHeight() / 2)).pad(900, 1000, 0, 0);
 		second.add(bar).width(bar.getWidth()*3).pad(900, 10, 0, 0);
 		
+		coinsTable = new Table();
+		coinsTable.add(coinImage).pad(0, 900, 900, 0);
+		coinsTable.add(coinsCount);
+		
+		menu.add(coinsTable);
 		menu.add(first);
 		menu.add(second);
+		
 		
 		menu.setFillParent(true);
 		stage.addActor(menu);
 		Gdx.input.setInputProcessor(stage);
 	}
 
-	private ImageButton setupImage(String imageString) {
-		Texture myTexture = new Texture(Gdx.files.internal(imageString));
-		TextureRegion myTextureRegion = new TextureRegion(myTexture);
-		TextureRegionDrawable myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
+	private ImageButton setupImage(TextureRegion textureRegion) {
+		TextureRegionDrawable myTexRegionDrawable = new TextureRegionDrawable(textureRegion);
 		return new ImageButton(myTexRegionDrawable);
 	}
 
 	public void render(float delta) {
 		bar.act(delta);
 		highscore.act(delta);
+		coinsCount.act(delta);
 		stage.act(delta);
 		stage.draw();
 	}
@@ -96,6 +117,10 @@ public class CarHud {
 	
 	public void setScore (long l) {
 		highscore.setText(String.valueOf(l));
+	}
+	
+	public void setCoins(int i) {
+		coinsCount.setText(String.valueOf(i));
 	}
 	
 	public void resize(int width, int height) {
