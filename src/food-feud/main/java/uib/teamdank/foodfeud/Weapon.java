99d9b7d99555a028af2dee6b3ed949a1e36c9cd6
Sturle Elvestad <sturle.elvestad@student.uib.no>
2017-05-04 13:59:38 +0200

    WIP3

--- src/food-feud/main/java/uib/teamdank/foodfeud/Weapon.java
+++ src/food-feud/main/java/uib/teamdank/foodfeud/Weapon.java
@@ -1,5 +1,7 @@
 package uib.teamdank.foodfeud;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import uib.teamdank.common.Item;
 
 /**
@@ -7,30 +9,57 @@ import uib.teamdank.common.Item;
  * weight and thrower) and inflicts damage on impact.
  */
 public class Weapon extends Item {
-	
-	public enum weaponProjectile{
-		Reg_Ballistic,
-		Burst_Ballistic,
-		//rpg etc..
-		Heavy_Ballistic,
-		Spray_Ballistic;
-		
+
+	private final int dmg, weight;
+	private final weaponProjectile proj;
+
+	private boolean damagedPlayer = false;
+
+	public enum weaponProjectile {
+		// light bullets like guns
+		LIGHT_BALLISTIC,
+		// like uzi, HK416.. etc
+		BURST_BALLISTIC,
+		// arching ballistics like rpg and grenades..(only exploding one)
+		HEAVY_BALLISTIC,
+		// shotgun etc
+		SPRAY_BALLISTIC;
+
 	}
-	
-	private weaponProjectile proj;
-	
+
 	/**
 	 * Creates a weapon that does no damage and weighs nothing.
+	 * 
 	 * @param name
-	 * @param descr weapon description
+	 * @param descr
+	 *            weapon description
 	 */
-    public Weapon(String name, String descr, weaponProjectile p) {
-        super(name, descr);
-        proj = p;
-    }
-	
-	public weaponProjectile getProjectileType(){
+	public Weapon(String name, String descr, int d, int w, TextureRegion texture, weaponProjectile p) {
+		super(name, descr);
+		this.dmg = d;
+		this.weight = w;
+		setTexture(texture);
+		proj = p;
+	}
+
+	public int getDamage() {
+		return dmg;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public weaponProjectile getProjectileType() {
 		return proj;
 	}
-	
-}
+
+	public void hitPlayer(Player player) {
+		if (!damagedPlayer) {
+			player.decreaseHealth(getDamage());
+			this.markForRemoval();
+			damagedPlayer = true;
+		}
+	}
+
+}
