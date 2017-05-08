0b9ab38bb1ca2d08a8fcc5d7ad62aa86b17e29c2
Markus Johan Ragnhildstveit <markus.ragnhildstveit@student.uib.no>
2017-05-08 14:11:22 +0200

    Forenklet PauseMenuScreen

--- src/car-game/main/java/uib/teamdank/cargame/gui/PauseMenuScreen.java
+++ src/car-game/main/java/uib/teamdank/cargame/gui/PauseMenuScreen.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -17,8 +14,9 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import uib.teamdank.cargame.CarGame;
 import uib.teamdank.common.Game;
+import uib.teamdank.common.gui.MenuScreen;
 
-public class PauseMenuScreen implements uib.teamdank.common.gui.PauseMenuScreen {
+public class PauseMenuScreen extends MenuScreen implements uib.teamdank.common.gui.PauseMenuScreen {
 	private static final String LOGO = "Images/CarGameLogo.png";
 	private static final String PAUSE = "Images/Buttons/bs_pause.png";
 	private static final String BACK = "Images/Buttons/bs_back.png";
@@ -61,39 +59,8 @@ public class PauseMenuScreen implements uib.teamdank.common.gui.PauseMenuScreen 
 		Gdx.input.setInputProcessor(stage);
 
 		// Her kommer alle lytterene for input
-		backButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Stage stage = event.getTarget().getStage();
-				Vector2 mouse = stage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
-
-				if (stage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
-					resume();
-				}
-			}
-		});
-
-		exitButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Stage stage = event.getTarget().getStage();
-				Vector2 mouse = stage.screenToStageCoordinates(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
-
-				if (stage.hit(mouse.x, mouse.y, true) == event.getTarget()) {
-					exitToStartMenu();
-				}
-			}
-		});
+		addButtonListener(backButton, () -> resume());
+		addButtonListener(exitButton, () -> exitToStartMenu());
 	}
 
 	// Setting the buttons up
