a78d1ec6e3430484addd3b53770ca4df6c879327
Philip Hoang <thao.hoang@student.uib.no>
2017-05-08 18:01:46 +0200

    SonarQ fix

--- src/food-feud/main/java/uib/teamdank/foodfeud/gui/WeaponMenu.java
+++ src/food-feud/main/java/uib/teamdank/foodfeud/gui/WeaponMenu.java
@@ -2,46 +2,36 @@ package uib.teamdank.foodfeud.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-import uib.teamdank.common.Game;
 import uib.teamdank.foodfeud.FoodFeud;
 
 public class WeaponMenu implements Screen{
-    private ImageButton menuButton;
+	private FoodFeud game;
 	private Texture myTexture;
 	private Table menu;
-	private Stage stage;
-	private Container<ImageButton> buttonCont;
-	
+	private Stage stage;	
 	private Table weapons;
 
-	private Array<Button> buttons;
-	private static final String ANANAS = "Weapons/Ananas.png";
-    private static final String CARROT = "Weapons/Carrot.png";
-    private static final String CHEESE = "Weapons/Cheese.png";
-    private static final String DONUT = "Weapons/donut.png";
+	private static final String ANANAS_PATH = "Weapons/Ananas.png";
+    private static final String CARROT_PATH = "Weapons/Carrot.png";
+    private static final String CHEESE_PATH = "Weapons/Cheese.png";
+    private static final String DONUT_PATH = "Weapons/donut.png";
 	
     private ImageButton weaponMenuButton;
 	private ImageButton ananas; 
 	private ImageButton carrot;
 	private ImageButton cheese;
 	private ImageButton donut;
-	private FoodFeud game;
 	
     private static final String MENU = "Images/Buttons/ff_menu.png";
 
@@ -49,17 +39,16 @@ public class WeaponMenu implements Screen{
 	public WeaponMenu() {
 		stage = new Stage(new FitViewport(1920, 1080));
 		
-        buttons = new Array<Button>();
 		menu = new Table(); 
 		weapons = new Table();
 		weaponMenuButton = setupButton(MENU);
 		menu.add(weaponMenuButton).height((float) (weaponMenuButton.getHeight() /4)).pad(980, 1670, 0, 0);
         addButtonListener();
 		
-		ananas = setupButton(ANANAS);
-        carrot = setupButton(CARROT);
-        cheese = setupButton(CHEESE);
-        donut = setupButton(DONUT);
+		ananas = setupButton(ANANAS_PATH);
+        carrot = setupButton(CARROT_PATH);
+        cheese = setupButton(CHEESE_PATH);
+        donut = setupButton(DONUT_PATH);
         
         addToWeapons();
         
