f55e069768e657b4a01f1b2b850e97482a819ce1
Eirik Strøm <eirikstrom1@gmail.com>
2017-05-10 00:13:28 +0200

    Ny versjon av DownpourSpawner

--- src/car-game/main/java/uib/teamdank/cargame/util/DownpourSpawner.java
+++ src/car-game/main/java/uib/teamdank/cargame/util/DownpourSpawner.java
@@ -1,11 +1,14 @@
 package uib.teamdank.cargame.util;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Queue;
+import uib.teamdank.common.Game;
 import uib.teamdank.common.GameObject;
 import uib.teamdank.common.gui.Layer;
 
 public class DownpourSpawner extends ScrollingSpawner {
-    private GameObject lastSpawn;
+    private Queue<Array<GameObject>> rows = new Queue<>();
 
     public DownpourSpawner(Layer layer, OrthographicCamera camera, WeatherGenerator weatherGenerator) {
         super(layer, camera, weatherGenerator);
@@ -21,12 +24,15 @@ public class DownpourSpawner extends ScrollingSpawner {
         return spawn;
     }
 
-    private void spawnRow(float y) {
+    private Array<GameObject> spawnRow(float y) {
+        Array<GameObject> row = new Array<>();
         float x = 0 - getCamera().viewportWidth / 2;
         while (x < getCamera().viewportWidth / 2) {
-            lastSpawn = createNewSpawn(x, y);
-            x += lastSpawn.getWidth();
+            GameObject sp = createNewSpawn(x, y);
+            row.add(sp);
+            x += sp.getWidth();
         }
+        return row;
     }
 
     @Override
@@ -34,14 +40,35 @@ public class DownpourSpawner extends ScrollingSpawner {
         if(getSpawns().size == 0) {
             float y = getCamera().position.y - getCamera().viewportHeight / 2;
             while(y < getCamera().position.y + getCamera().viewportHeight / 2) {
-                spawnRow(y);
-                y += lastSpawn.getHeight();
+                Array<GameObject> row = spawnRow(y);
+                rows.addLast(row);
+                y += row.get(0).getHeight();
             }
         }
 
-        else if(lastSpawn.getPosition().y - lastSpawn.getHeight() < getCamera().position.y - getCamera().viewportHeight / 2) {
-            spawnRow(lastSpawn.getPosition().y + lastSpawn.getHeight());
-            deleteOldStructures();
+        else {
+            boolean move = false;
+            Array<GameObject> row = rows.first();
+
+            if(row.get(0).getPosition().y + row.get(0).getHeight() < getCamera().position.y - getCamera().viewportHeight / 2) {
+                move = true;
+            }
+
+            if(move) {
+                for(int i = 0; i < row.size; i++) {
+                    if(rows.size > 1)
+                        row.get(i).getPosition().y = rows.get(1).get(0).getPosition().y + row.get(i).getHeight();
+                    else
+                        row.get(i).getPosition().y += row.get(i).getHeight();
+                    row.get(i).getPosition().x = row.get(i).getWidth()*i - getCamera().position.x - getCamera().viewportWidth / 2;
+                }
+
+                rows.addLast(rows.removeFirst());
+            }
+
+            if(rows.first().get(0).getPosition().y + rows.first().get(0).getHeight() < getCamera().position.y + getCamera().viewportHeight / 2) {
+                rows.addFirst(spawnRow(rows.first().get(0).getPosition().y + rows.first().get(0).getHeight()));
+            }
         }
     }
 }
