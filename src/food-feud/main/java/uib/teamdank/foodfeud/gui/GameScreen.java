0dcac983e55d39995b7413ea09bdbb181f2208ac
Elias Siljan <Elias.Siljan@student.uib.no>
2017-05-10 16:22:33 +0200

    resove merge conflict

--- src/food-feud/main/java/uib/teamdank/foodfeud/gui/GameScreen.java
+++ src/food-feud/main/java/uib/teamdank/foodfeud/gui/GameScreen.java
@@ -188,7 +188,6 @@ public class GameScreen extends uib.teamdank.common.gui.GameScreen {
 				Vector3 aim3D = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
 				Vector2 aim = new Vector2(aim3D.x, aim3D.y);
 				aim.sub(activePlayer.getPosition());
-				System.out.println(elapsedTime * 100);
 				if(match.CURRENT_AMMO_COUNT>0){
 					activePlayer.fireWeapon(playerLayer, level.getWorld(), aim.nor(), elapsedTime * 100);
 					match.CURRENT_AMMO_COUNT--;
